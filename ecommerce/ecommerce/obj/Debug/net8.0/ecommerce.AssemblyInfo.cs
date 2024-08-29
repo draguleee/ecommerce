129d4851aebad7411072f90753b6d369e5a5f818
draguleee <aandreid14@gmail.com>
2024-08-29 18:19:00 +0300

    Modify .yml file

--- ecommerce/ecommerce/obj/Debug/net8.0/ecommerce.AssemblyInfo.cs
+++ ecommerce/ecommerce/obj/Debug/net8.0/ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+499f6022871f68ddfb897d937bbff22fb0930524")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e924d5e76fe5c1ea03db721ec623477dd8df669")]
 [assembly: System.Reflection.AssemblyProductAttribute("ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
